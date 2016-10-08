6cae7e04d29a5135c7f8770f45b4946de0ae3d61
ted <tedhoryczuniv@outlook.com>
2016-10-07 20:44:56 -0400

    added one rep calculation and test

--- app/src/test/java/com/repmax/devlanding/onerepmaxcalculator/ExampleUnitTest.java
+++ app/src/test/java/com/repmax/devlanding/onerepmaxcalculator/ExampleUnitTest.java
@@ -1,5 +1,10 @@
 package com.repmax.devlanding.onerepmaxcalculator;
 
+import com.repmax.devlanding.onerepmaxcalculator.calculator.OneRepMaxTemplate;
+import com.repmax.devlanding.onerepmaxcalculator.calculator.PercentRepMax;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,4 +17,12 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void repTest(){
+        OneRepMaxTemplate oneRep = new PercentRepMax();
+        int[] rep = oneRep.determineRepMaxes(45, 10);
+        Assert.assertEquals(49,  rep[5]);
+    }
+
 }
